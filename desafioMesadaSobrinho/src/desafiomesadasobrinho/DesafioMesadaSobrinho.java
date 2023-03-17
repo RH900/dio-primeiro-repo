d654edbe7401131ca79811ca169e0bb9235fbf84
RH900 <rubens-900@hotmail.com>
2023-03-17 01:02:13 -0300

    Desafio - Mesada do sobrinho - Resolvido

---
+++ desafioMesadaSobrinho/src/desafiomesadasobrinho/DesafioMesadaSobrinho.java
@@ -0,0 +1,16 @@
+package desafiomesadasobrinho;
+import java.util.Scanner;
+
+public class DesafioMesadaSobrinho {
+
+    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+        mesada = entrada * mesada;
+        System.out.println(mesada);
+        
+    }
+    
+}
